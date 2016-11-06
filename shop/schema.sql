6ac4ba846d259adc447f1f0a174e73a7c7511e05
KovalKt <katrysasonce@gmail.com>
2016-11-06 15:17:52 +0200

    remove product ID from page; modify images

--- shop/schema.sql
+++ shop/schema.sql
@@ -2,5 +2,7 @@ drop table if exists product;
 create table product (
   id integer primary key autoincrement,
   name text not null,
-  price integer not null
+  price integer not null,
+  picture_name text not null,
+  amount_on_stock integer default 0
 );
