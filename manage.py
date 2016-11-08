a0b26c28c31f8e76a56d39e9396ea66700890109
KovalKt <katrysasonce@gmail.com>
2016-11-08 08:39:56 +0200

    add .gitignore; clean

--- manage.py
+++ manage.py
@@ -19,13 +19,17 @@ def add_records_for_test():
     from shop import app, db
     from shop.models import Product
     with app.app_context():
-        product1 = Product('Milk', 10, "/static/milk.jpg", 10)
-        product2 = Product('Super water', 500, "/static/water.jpg", 3)
-        product3 = Product('Beef meat 1kg', 70, "/static/meat.jpg", 7)
+        products = [Product('Milk', 10, "/static/milk.jpg", 10)]
+        products.append(Product('Super water', 500, "/static/water.jpg", 3))
+        products.append(Product('Beef meat 1kg', 70, "/static/meat.jpg", 7))
+        products.append(Product('Candy', 7, "/static/meat.jpg", 50))
+        products.append(Product('Orange 1kg', 20, "/static/meat.jpg", 24))
+        products.append(Product('Apple 1kg', 14, "/static/meat.jpg", 20))
+        products.append(Product('Power bar', 25, "/static/meat.jpg", 10))
 
-        db.session.add(product1)
-        db.session.add(product2)
-        db.session.add(product3)
+        for product in products:
+            db.session.add(product)
+
         db.session.commit()
 
 @manager.command
@@ -35,8 +39,6 @@ def clean_tables():
     from shop.models import Product
     with app.app_context():
         db.drop_all()
-        # Product.query.delete()
-
         db.session.commit()
 
 @manager.command
